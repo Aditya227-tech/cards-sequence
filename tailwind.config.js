d91abba6acb242c3a0e18b8b9d226b273c6a17dc
Aditya227-tech <141914748+Aditya227-tech@users.noreply.github.com>
2024-11-08 18:07:53 +0000

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,49 @@ module.exports = {
         'bounce-slight': {
           '0%, 100%': { transform: 'translateY(-2%)' },
           '50%': { transform: 'translateY(0)' },
+        },
+        rotate: {
+          '0%': { transform: 'rotate(0deg) scale(0.8)' },
+          '50%': { transform: 'rotate(360deg) scale(1.2)' },
+          '100%': { transform: 'rotate(720deg) scale(0.8)' },
+        },
+        ball1: {
+          '0%': { 
+            boxShadow: '30px 0 0 #ff3d00',
+            marginBottom: '10px'
+          },
+          '50%': { 
+            boxShadow: '0 0 0 #ff3d00',
+            marginBottom: '0',
+            transform: 'translate(15px, 15px)'
+          },
+          '100%': { 
+            boxShadow: '30px 0 0 #ff3d00',
+            marginBottom: '10px'
+          },
+        },
+        ball2: {
+          '0%': { 
+            boxShadow: '30px 0 0 #fff',
+            marginTop: '0'
+          },
+          '50%': { 
+            boxShadow: '0 0 0 #fff',
+            marginTop: '-20px',
+            transform: 'translate(15px, 15px)'
+          },
+          '100%': { 
+            boxShadow: '30px 0 0 #fff',
+            marginTop: '0'
+          },
         }
       },
       animation: {
         slideIn: 'slideIn 0.5s ease-out forwards',
         'bounce-slight': 'bounce-slight 1s infinite',
+        'loader-rotate': 'rotate 1s infinite',
+        'loader-ball1': 'ball1 1s infinite',
+        'loader-ball2': 'ball2 1s infinite'
       },
     },
   },
